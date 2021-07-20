95c7daa0c55d78a95f22b39e4ca837991b27e5f1
TyhypkoIryna <83498723+TyhypkoIryna@users.noreply.github.com>
2021-07-20 22:33:01 +0300

    Update SignIn.spec.js

--- specs/SignIn.spec.js
+++ specs/SignIn.spec.js
@@ -1,46 +1,28 @@
-const assert = require('assert');
-const {expect} = require('chai')
+
+const { App } = require('../src/pages');
+const rundomNumber = () => Date.now();
 //  Тест на логін
 // [x] перехід по урлу
 // [x] заповнити форму
 // [x] натиснути кнопку рег.
 // [x] перевірка результату
 
-// перехід по урлу
+const app = new App();
+
 describe('SignIn:', function () {
-  xit('should be able to SignIn', async function (){
+  beforeEach(async function(){
     await browser.setWindowSize(1440, 960);
     await browser.url('http://46.101.234.121/sign-in');
+  });
+  this.afterEach(async function(){
+    await browser.reloadSession();
+  });
 
-// заповнити форму 
-    const emailField = await $('input[name="email"]');
-    const passwordField = await $('input[name="password"]');
-    const signUpButton = await $('button');
+  it('should be able to SignIn', async function (){
 
-
-    await emailField.waitForDisplayed({ timeout: 5000 });
-    await emailField.setValue(`john_admin1@admin.com`);
-
-    await passwordField.waitForDisplayed({ timeout: 5000 });
-    await passwordField.setValue('Pa55word');
-
-// натиснути кнопку рег.
-    await signUpButton.waitForDisplayed({ timeout: 5000 });
-        await signUpButton.click();
-
-// перевірка результату
-        await browser.waitUntil( 
-          async function () {
-            const url = await browser.getUrl();
-            return url === 'http://46.101.234.121/doctors';
-          },
-          { timeout: 5000 },
-        );
-        
-        const url = await browser.getUrl();
-        expect(url).to.be.eql('http://46.101.234.121/doctors');
-
-        await browser.reloadSession();
+await app.authPage.logIn({ 
+  email: `marcus${rundomNumber()}@gmail.com`,
+  password: 'Pa55word',})
 });
 
 });
